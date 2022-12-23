5a462e28b02d00e872b2b55ec1a4ff55a1a97aa5
Sanjaythanneeru <118734422+Sanjaythanneeru@users.noreply.github.com>
2022-12-23 14:53:01 +0530

    Create Insertion Sort.py

---
+++ 1.Intro to Python/Searching and Sorting/Insertion Sort.py
@@ -0,0 +1,31 @@
+Provided with a random integer array/list(ARR) of size N, you have been required to sort this array using 'Insertion Sort'.
+ Note:
+Change in the input array/list itself. You don't need to return or print the elements.
+ Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+First line of each test case or query contains an integer 'N' representing the size of the array/list.
+
+Second line contains 'N' single space separated integers representing the elements in the array/list.
+Output Format :
+For each test case, print the elements of the array/list in sorted order separated by a single space.
+
+Output for every test case will be printed in a separate line.
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^3
+Time Limit: 1 sec
+  
+  
+SOLUTION
+=========================================
+from sys import stdin
+
+def insertionSort(arr, n) : 
+    k=0
+    for i in range(1,n):
+        key=i
+        for j in range(i-1,-1,-1):
+            if arr[j]>arr[key]:
+                arr[key], arr[j] =arr[j], arr[key]
+                key=j
