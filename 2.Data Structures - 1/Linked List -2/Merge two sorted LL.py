f36836b6f8989b6bc963f085d14d7cd63bd29d73
Sanjaythanneeru <118734422+Sanjaythanneeru@users.noreply.github.com>
2022-12-31 21:11:51 +0530

    Create Merge two sorted LL.py

---
+++ 2.Data Structures - 1/Linked List -2/Merge two sorted LL.py
@@ -0,0 +1,124 @@
+You have been given two sorted(in ascending order) singly linked lists of integers.
+Write a function to merge them in such a way that the resulting singly linked list is also sorted(in ascending order) and return the new head to the list.
+Note :
+Try solving this in O(1) auxiliary space.
+
+No need to print the list, it has already been taken care.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first line of each test case or query contains the elements of the first sorted singly linked list separated by a single space. 
+
+The second line of the input contains the elements of the second sorted singly linked list separated by a single space. 
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+Output :
+For each test case/query, print the resulting sorted singly linked list, separated by a single space.
+
+Output for every test case will be printed in a seperate line.
+Constraints :
+1 <= t = 10^2
+0 <= N <= 10 ^ 4
+0 <= M <= 10 ^ 4
+Where N and M denote the sizes of the singly linked lists. 
+
+Time Limit: 1sec
+Sample Input 1 :
+1
+2 5 8 12 -1
+3 6 9 -1
+Sample Output 1 :
+2 3 5 6 8 9 12 
+
+
+
+SOLUTION
+===============================================
+from sys import stdin
+
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+def mergeTwoSortedLinkedLists(head1, head2):
+    finalHead, finalTail = None, None
+    if head1 is None:
+        return head2
+    if head2 is None:
+        return head1
+    if head1.data <= head2.data:
+        finalHead = head1
+        finalTail = head1
+        head1= head1.next
+    else:
+        finalHead = head2
+        finalTail = head2
+        head2 = head2.next
+    while(head1 != None and head2 != None):
+        if head1.data <= head2.data:
+            finalTail.next = head1
+            finalTail = finalTail.next
+            head1 = head1.next
+        else:
+            finalTail.next = head2
+            finalTail = finalTail.next
+            head2 = head2.next
+    if head1 is not None:
+        finalTail.next = head1
+    if head2 is not None:
+        finalTail.next = head2
+    return finalHead
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+
+
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+# Main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head1 = takeInput()
+    head2 = takeInput()
+
+    newHead = mergeTwoSortedLinkedLists(head1, head2)
+    printLinkedList(newHead)
+
+    t -= 1
