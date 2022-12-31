d2734a479fbdd2841b78419e7749ec157efc3a20
Sanjaythanneeru <118734422+Sanjaythanneeru@users.noreply.github.com>
2022-12-31 21:17:01 +0530

    Create Swap two Nodes of LL.py

---
+++ 2.Data Structures - 1/Linked List -2/Swap two Nodes of LL.py
@@ -0,0 +1,128 @@
+You have been given a singly linked list of integers along with two integers, 'i,' and 'j.' Swap the nodes that are present at the 'i-th' and 'j-th' positions.
+Note :
+Remember, the nodes themselves must be swapped and not the datas.
+
+No need to print the list, it has already been taken care. Only return the new head to the list.
+Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first line of each test case or query contains the elements of the singly linked list separated by a single space.
+
+The second line of input contains two integer values 'i,' and 'j,' respectively. A single space will separate them.
+ Remember/consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element
+Output format :
+For each test case/query, print the elements of the updated singly linked list.
+
+Output for every test case will be printed in a seperate line.
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Where M is the size of the singly linked list.
+0 <= i < M
+0 <= j < M
+
+Time Limit: 1sec
+Sample Input 1 :
+1
+3 4 5 2 6 1 9 -1
+3 4
+Sample Output 1 :
+3 4 5 6 2 1 9 
+
+
+
+SOLUTION
+=================================
+from sys import stdin
+
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+def swapNodes(head, i, j) :
+    if i==j:
+        return head
+    if j<i:
+        i,j = j,i
+    prev1, curr1 = None, head
+
+    for k in range(i):
+        prev1 = curr1
+        curr1 = curr1.next
+
+    prev2, curr2 = prev1, curr1
+
+    for k in range(i,j):
+        prev2 = curr2
+        curr2 = curr2.next
+
+    if prev1 is not None:
+        prev1.next = curr2
+    else:
+        head = curr2
+    
+    if prev2 is not None:
+        prev2.next = curr1
+    else:
+        head = curr1
+
+    curr1.next,curr2.next = curr2.next, curr1.next
+
+    return head
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+
+
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head = takeInput()
+    i_j = stdin.readline().strip().split(" ")
+
+    i = int(i_j[0])
+    j = int(i_j[1])
+
+    newHead = swapNodes(head, i, j)
+    printLinkedList(newHead)
+
+    t -= 1
