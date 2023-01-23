0cfd950b3b1b0af5ff5509908016c56a34f14bb9
Sanjaythanneeru <118734422+Sanjaythanneeru@users.noreply.github.com>
2023-01-23 22:40:20 +0530

    Create Print Levelwise.py

---
+++ 3.Data Structures - 2/Binary Trees -2/Print Levelwise.py
@@ -0,0 +1,110 @@
+For a given a Binary Tree of type integer, print the complete information of every node, when traversed in a level-order fashion.
+To print the information of a node with data D, you need to follow the exact format :
+
+           D:L:X,R:Y
+
+Where D is the data of a node present in the binary tree. 
+X and Y are the values of the left(L) and right(R) child of the node.
+Print -1 if the child doesn't exist.
+Note: There is no space in between while printing the information for each node.
+Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+Information of all the nodes in the Binary Tree will be printed on a different line where each node will follow a format of D:L:X,R:Y, without any spaces in between.
+Constraints:
+1 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+ Sample Output 1:
+8:L:3,R:10
+3:L:1,R:6
+10:L:-1,R:14
+1:L:-1,R:-1
+6:L:4,R:7
+14:L:13,R:-1
+4:L:-1,R:-1
+7:L:-1,R:-1
+13:L:-1,R:-1
+      
+SOLUTION
+###################################################################
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def printLevelWise(root):
+    if root == None:
+        return
+    q = queue.Queue()
+
+    q.put(root)
+    while (not(q.empty())):
+        curr_node = q.get()
+        print(str(curr_node.data) + ":", end="")
+        if curr_node.left != None:
+            q.put(curr_node.left)
+            print("L:"+str(curr_node.left.data)+",", end="")
+        else:
+            print("L:"+str(-1)+",", end="")
+        if curr_node.right != None:
+            q.put(curr_node.right)
+            print("R:"+str(curr_node.right.data))
+        else:
+            print("R:"+str(-1))
+    return root
+
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+    
+    length = len(levelOrder)
+
+    if length == 1 :
+        return None
+    
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left =leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right =rightNode
+            q.put(rightNode)
+
+    return root
+
+
+# Main
+root = takeInput()
+printLevelWise(root)
