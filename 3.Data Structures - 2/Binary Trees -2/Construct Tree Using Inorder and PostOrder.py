f62a5fdf9895c5563da6d5968c626bac469765d5
Sanjaythanneeru <118734422+Sanjaythanneeru@users.noreply.github.com>
2023-01-23 22:42:19 +0530

    Create Construct Tree Using Inorder and PostOrder.py

---
+++ 3.Data Structures - 2/Binary Trees -2/Construct Tree Using Inorder and PostOrder.py
@@ -0,0 +1,115 @@
+For a given postorder and inorder traversal of a Binary Tree of type integer stored in an array/list, create the binary tree using the given two arrays/lists. You just need to construct the tree and return the root.
+Note:
+Assume that the Binary Tree contains only unique elements. 
+Input Format:
+The first line of input contains an integer N denoting the size of the list/array. It can also be said that N is the total number of nodes the binary tree would have.
+
+The second line of input contains N integers, all separated by a single space. It represents the Postorder-traversal of the binary tree.
+
+The third line of input contains N integers, all separated by a single space. It represents the inorder-traversal of the binary tree.
+Output Format:
+The given input tree will be printed in a level order fashion where each level will be printed on a new line. 
+Elements on every level will be printed in a linear fashion. A single space will separate them.
+Constraints:
+1 <= N <= 10^4
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+7
+4 5 2 6 7 3 1 
+4 2 5 1 6 3 7 
+Sample Output 1:
+1 
+2 3 
+4 5 6 7 
+
+
+SOLUTION
+#######################################################################
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+
+#Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+
+def buildTree(postOrder, inOrder, n) :
+    if n==0:
+        return None
+    
+    rootData = postOrder[-1]
+    root = BinaryTreeNode(rootData)
+
+    leftInorder = inOrder[0:inOrder.index(rootData)]
+    rightInorder = inOrder[inOrder.index(rootData)+1:]
+
+    leftPostorder = postOrder[0:len(leftInorder)]
+    rightPostorder = postOrder[len(leftInorder):-1]
+
+    leftSubtree = buildTree(leftPostorder,leftInorder,len(leftInorder))
+    rightSubtree = buildTree(rightPostorder,rightInorder,n-len(leftInorder)-1)
+
+    root.left = leftSubtree
+    root.right = rightSubtree
+
+    return root 
+
+
+'''-------------------------- Utility Functions --------------------------'''
+
+def printLevelWise(root):
+    if root is None :
+        return
+
+    pendingNodes = queue.Queue()
+    pendingNodes.put(root)
+    pendingNodes.put(None)
+
+    while not pendingNodes.empty(): 
+        frontNode = pendingNodes.get()
+    
+        if frontNode is None :
+            print()
+            
+            if not pendingNodes.empty() :
+                pendingNodes.put(None)
+                
+        else :
+            print(frontNode.data, end = " ")
+            
+            if frontNode.left is not None :
+                pendingNodes.put(frontNode.left)
+                
+                
+            if frontNode.right is not None :
+                pendingNodes.put(frontNode.right)
+
+
+                
+
+#Taking level-order input using fast I/O method
+def takeInput():
+    n = int(stdin.readline().strip())
+
+    if n == 0 :
+        return list(), list(), 0
+
+    postOrder = list(map(int, stdin.readline().strip().split(" ")))
+    inOrder = list(map(int, stdin.readline().strip().split(" ")))
+
+    return postOrder, inOrder, n
+
+
+# Main
+postOrder, inOrder, n = takeInput()
+root = buildTree(postOrder, inOrder, n)
+printLevelWise(root)
